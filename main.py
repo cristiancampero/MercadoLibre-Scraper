1e1433b7c4e36475385b0a14f4087d080e61daa2
pabloalaniz <pabloalaniz@culturainteractiva.com>
2023-10-12 20:43:04 -0300

    Refactor con mismo funcionamiento

--- main.py
+++ main.py
@@ -2,139 +2,157 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
+from tqdm import tqdm
+import time
 
-class Scraper():
+
+class MercadoLibreScraper:
+
+    BASE_URL = 'https://listado.mercadolibre.com.{domain}/'
+    DATA_DIRECTORY = "data"
+    CSV_SEPARATOR = ";"
+    PAGE_INCREMENT = 50
+    MAX_PAGES = 200
+
+    def __init__(self):
+        self.data = []
+
+    def get_page_content(self, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.text
+        except requests.RequestException as e:
+            raise Exception(f"Error al obtener la página {url}: {e}")
+
+    def extract_post_data(self, post):
+        # Simplificar y mejorar legibilidad
+        title_element = post.find('h2')
+        price_element = post.find('span', class_='andes-money-amount__fraction')
+        post_link_element = post.find("a")
+        img_element = post.find("img")
+
+        data = {
+            'title': title_element.text if title_element else None,
+            'price': price_element.text if price_element else None,
+            'post link': post_link_element['href'] if post_link_element else None,
+            'image link': img_element.get('data-src', img_element.get('src')) if img_element else None
+        }
+        return data
+
+    def get_total_results(self, soup):
+        results_element = soup.find('span', class_='ui-search-search-result__quantity-results')
+        if results_element:
+            return int(results_element.text.split()[0].replace('.', '').replace(',', ''))
+        return 0  # o cualquier valor predeterminado
+
+    def scrape_page(self, url):
+        page_content = self.get_page_content(url)
+        if not page_content:
+            return False
+
+        soup = BeautifulSoup(page_content, 'html.parser')
+        content = soup.find_all('li', class_='ui-search-layout__item')
+        for post in content:
+            post_data = self.extract_post_data(post)
+            self.data.append(post_data)
+        return True
+
+    def scrape_product(self, domain, product_name, user_scraping_limit=1000):
+        cleaned_name = product_name.replace(" ", "-").lower()
+        base_url = self.BASE_URL.format(domain=domain)
+
+        # Obtener el contenido de la primera página para extraer el número total de resultados
+        page_content = self.get_page_content(base_url + cleaned_name)
+        soup = BeautifulSoup(page_content, 'html.parser')
+        total_results = self.get_total_results(soup)
+
+        # Usa el límite más pequeño: total_results, user_scraping_limit, o el límite por defecto (1000)
+        scraping_limit = min(total_results, user_scraping_limit, 1000)
+
+        total_products_scraped = 0
+        with tqdm(total=self.MAX_PAGES, desc="Páginas escrapeadas") as progress_bar:
+            for i in range(0, self.MAX_PAGES):
+                if total_products_scraped >= scraping_limit:
+                    print(f"\nSe alcanzó el límite de {scraping_limit} productos. Terminando.")
+                    break
+
+                url = f"{base_url}{cleaned_name}_Desde_{(i * self.PAGE_INCREMENT) + 1}_NoIndex_True"
+                loop_start_time = time.time()
+
+                if not self.scrape_page(url):
+                    print("\nTerminó el scraping.")
+                    break
+
+                loop_elapsed_time = time.time() - loop_start_time
+                total_products_scraped += len(self.data)
+                progress_bar.update(1)
+                progress_bar.set_postfix({"Tiempo loop": f"{loop_elapsed_time:.2f} s"})
+
+    def export_to_csv(self, product_name):
+        filename = f"{product_name}_scraped_data.csv"
+        if not os.path.exists(self.DATA_DIRECTORY):
+            os.makedirs(self.DATA_DIRECTORY)
+        df = pd.DataFrame(self.data)
+        file_path = os.path.join(self.DATA_DIRECTORY, filename)
+        df.to_csv(file_path, sep=self.CSV_SEPARATOR)
+
+
+class UserInterface:
+
+    BASE_URLS = {
+        1: {'country': 'Argentina', 'domain': 'ar'},
+        2: {'country': 'Bolivia', 'domain': 'bo'},
+        3: {'country': 'Brasil', 'domain': 'br'},
+        4: {'country': 'Chile', 'domain': 'cl'},
+        5: {'country': 'Colombia', 'domain': 'co'},
+        6: {'country': 'Costa Rica', 'domain': 'cr'},
+        7: {'country': 'Dominicana', 'domain': 'do'},
+        8: {'country': 'Ecuador', 'domain': 'ec'},
+        9: {'country': 'Guatemala', 'domain': 'gt'},
+        10: {'country': 'Honduras', 'domain': 'hn'},
+        11: {'country': 'México', 'domain': 'mx'},
+        12: {'country': 'Nicaragua', 'domain': 'ni'},
+        13: {'country': 'Panamá', 'domain': 'pa'},
+        14: {'country': 'Paraguay', 'domain': 'py'},
+        15: {'country': 'Perú', 'domain': 'pe'},
+        16: {'country': 'Salvador', 'domain': 'sv'},
+        17: {'country': 'Uruguay', 'domain': 'uy'},
+        18: {'country': 'Venezuela', 'domain': 've'},
+    }
+
+    def __init__(self, scraper):
+        self.scraper = scraper
 
     def menu(self):
-        menu = ("""
-    Escoge el país:
-    1. Argentina
-    2. Bolivia
-    3. Brasil
-    4. Chile
-    5. Colombia
-    6. Costa Rica
-    7. Dominicana
-    8. Ecuador
-    9. Guatemala
-    10. Honduras
-    11. México
-    12. Nicaragua
-    13. Panamá
-    14. Paraguay
-    15. Perú
-    16. Salvador
-    17. Uruguay
-    18. Venezuela
-        """)
+        print("\nSeleccioná el país:")
+        for number, country_info in self.BASE_URLS.items():
+            print(f"{number}. {country_info['country']}")
 
-        valid_options = list(range(1, 19))
-
-        while True:
-            print(menu)
+        try:
             opcion = int(input('Número de país (Ejemplo: 5): '))
+        except ValueError:
+            print("Por favor, elegí un número válido.")
+            return
 
-            if opcion in valid_options:
+        if opcion not in self.BASE_URLS:
+            print("Elegí un número válido del 1 al 18.")
+            return
 
-                urls = {
-                1: 'https://listado.mercadolibre.com.ar/',
-                2: 'https://listado.mercadolibre.com.bo/',
-                3: 'https://listado.mercadolibre.com.br/',
-                4: 'https://listado.mercadolibre.cl/',
-                5: 'https://listado.mercadolibre.com.co/',
-                6: 'https://listado.mercadolibre.com.cr/',
-                7: 'https://listado.mercadolibre.com.do/',
-                8: 'https://listado.mercadolibre.com.ec/',
-                9: 'https://listado.mercadolibre.com.gt/',
-                10: 'https://listado.mercadolibre.com.hn/',
-                11: 'https://listado.mercadolibre.com.mx/',
-                12: 'https://listado.mercadolibre.com.ni/',
-                13: 'https://listado.mercadolibre.com.pa/',
-                14: 'https://listado.mercadolibre.com.py/',
-                15: 'https://listado.mercadolibre.com.pe/',
-                16: 'https://listado.mercadolibre.com.sv/',
-                17: 'https://listado.mercadolibre.com.uy/',
-                18: 'https://listado.mercadolibre.com.ve/',
-                }
-
-                self.base_url = urls[opcion]
-                break
-            else:
-                print("Escoge un número del 1 al 18")
-
-
-
-    def scraping(self):
-        # User search
+        domain = self.BASE_URLS[opcion]['domain']
         product_name = input("\nProducto: ")
-        # Clean the user input
-        cleaned_name = product_name.replace(" ", "-").lower()
-        # Create the urls to scrap
-        urls = [self.base_url + cleaned_name]
 
-        page_number = 50
-        for i in range(0, 10000, 50):
-            urls.append(f"{self.base_url}{cleaned_name}_Desde_{page_number + 1}_NoIndex_True")
-            page_number += 50
+        try:
+            scraping_limit = int(input(f"\nCantidad máxima de productos a escrapear (por defecto: 1000): "))
+        except ValueError:
+            print("Valor inválido. Se usará el límite por defecto de 1000.")
+            scraping_limit = 1000
 
-        # create a list to save the data
-        self.data = []
-        # create counter
-        c = 1
-            
-        # Iterate over each url
-        for i, url in enumerate(urls, start=1):
+        self.scraper.scrape_product(domain, product_name, scraping_limit)
+        self.scraper.export_to_csv(product_name)
 
-            # Get the html of the page
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-                
-            # take all posts
-            content = soup.find_all('li', class_='ui-search-layout__item')
-            
-            # Check if there's no content to scrape
-            if not content:
-                print("\nTermino el scraping.")
-                break
-
-            print(f"\nScrapeando pagina numero {i}. {url}")
-            
-            
-            # iteration to scrape posts
-            for post in content:
-                # get the title
-                title = post.find('h2').text
-                # get the price
-                price = post.find('span', class_='andes-money-amount__fraction').text
-                # get the url post
-                post_link = post.find("a")["href"]
-                # get the url image
-                try:
-                    img_link = post.find("img")["data-src"]
-                except:
-                    img_link = post.find("img")["src"]
-                
-                # show the data already scraped
-                # print(f"{c}. {title}, {price}, {post_link}, {img_link}")
-
-                # save in a dictionary
-                post_data = {
-                    "title": title,
-                    "price": price,
-                    "post link": post_link,
-                    "image link": img_link            
-                }
-                # save the dictionaries in a list
-                self.data.append(post_data)
-                c += 1
-
-    def export_to_csv(self):
-        # export to a csv file
-        df = pd.DataFrame(self.data)
-        df.to_csv(r"data/mercadolibre_scraped_data.csv", sep=";")
 
 if __name__ == "__main__":
-    s = Scraper()
-    s.menu()
-    s.scraping()
-    s.export_to_csv()
+    scraper = MercadoLibreScraper()
+    ui = UserInterface(scraper)
+    ui.menu()
